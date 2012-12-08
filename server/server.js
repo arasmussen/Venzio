99371f5712c8755b23d3f6fda64c3526650eb43f
Andrew Rasmussen <arasmussen@katworks.com>
2012-12-08 06:52:03 -0500

    update server

--- server/server.js
+++ server/server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
-}).listen(82, "gfx.rasmuzen.com");
+}).listen(82, "localhost");
