776ab31c7da313c7b56bbbc070901986a63bb3f6
Andrew Rasmussen <arasmussen@katworks.com>
2013-02-16 13:54:48 -0800

    Fix texture seams!

--- webroot/client/shaders/terrain-fs.glsl
+++ webroot/client/shaders/terrain-fs.glsl
@@ -8,23 +8,24 @@ varying vec2 texCoord;
 varying float y;
 
 void main(void) {
+  vec4 dirt = texture2D(dirt_texture, texCoord);
+  vec4 grass = texture2D(grass_texture, texCoord);
+  vec4 snow = texture2D(snow_texture, texCoord);
+  float dirt_weight = 0.0;
+  float grass_weight = 0.0;
+  float snow_weight = 0.0;
   if (y < -5.0) {
-    gl_FragColor = texture2D(dirt_texture, texCoord);
+    dirt_weight = 1.0;
   } else if (y < 0.0) {
-    gl_FragColor = mix(
-      texture2D(dirt_texture, texCoord),
-      texture2D(grass_texture, texCoord),
-      (y + 5.0) / 5.0
-    );
+    grass_weight = (y + 5.0) / 5.0;
+    dirt_weight = 1.0 - grass_weight;
   } else if (y < 3.0) {
-    gl_FragColor = texture2D(grass_texture, texCoord);
+    grass_weight = 1.0;
   } else if (y < 5.0) {
-    gl_FragColor = mix(
-      texture2D(grass_texture, texCoord),
-      texture2D(snow_texture, texCoord),
-      (y - 3.0) / 2.0
-    );
+    snow_weight = (y - 3.0) / 2.0;
+    grass_weight = 1.0 - snow_weight;
   } else {
-    gl_FragColor = texture2D(snow_texture, texCoord);
+    snow_weight = 1.0;
   }
+  gl_FragColor = dirt * dirt_weight + grass * grass_weight + snow * snow_weight;
 }
