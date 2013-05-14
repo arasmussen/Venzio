0987814bfa3bf39f6b9084086996ce7030cc1469
Andrew <arasmussen@katworks.com>
2013-05-13 18:42:08 -0700

    fix count_lines

--- scripts/count_lines.sh
+++ scripts/count_lines.sh
@@ -10,17 +10,20 @@ GFX='..'
 
 LIB=$(find ${GFX}/root/client/lib -name \*.js | xargs wc -l | grep total | awk '{print $1}')
 JS=$(find ${GFX} -name \*.js | xargs wc -l | grep total | awk '{print $1}')
+MODULES_JS=$(find ${GFX}/node_modules -name \*.js | xargs wc -l | grep total | awk '{print $1}')
 GLSL=$(find ${GFX} -name \*.glsl | xargs wc -l | grep total | awk '{print $1}')
-CSS=$(find ${GFX} -name \*.css | xargs wc -l | awk '{print $1}')
-HTML=$(find ${GFX} -name \*.html | xargs wc -l | awk '{print $1}')
-SH=$(find ${GFX} -name \*.sh | xargs wc -l | awk '{print $1}')
+CSS=$(find ${GFX} -name \*.css | xargs wc -l | grep total | awk '{print $1}')
+MODULES_CSS=$(find ${GFX}/node_modules -name \*.css | xargs wc -l | grep total | awk '{print $1}')
+HTML=$(find ${GFX} -name \*.html | xargs wc -l | grep total | awk '{print $1}')
+MODULES_HTML=$(find ${GFX}/node_modules -name \*.html | xargs wc -l | grep total | awk '{print $1}')
+SH=$(find ${GFX} -name \*.sh | xargs wc -l | grep total | awk '{print $1}')
 
-echo "JS: $(expr $JS - $LIB)"
+echo "JS: $(expr $JS - $LIB - $MODULES_JS)"
 echo "GLSL: $GLSL"
-echo "CSS: $CSS"
-echo "HTML: $HTML"
+echo "CSS: $(expr $CSS - $MODULES_CSS)"
+echo "HTML: $(expr $HTML - $MODULES_HTML)"
 echo "SH: $SH"
 echo "-----"
-echo "Total: $(expr $JS - $LIB + $GLSL + $CSS + $HTML + $SH)"
+echo "Total: $(expr $JS + $GLSL + $CSS + $HTML + $SH - $LIB - $MODULES_JS - $MODULES_CSS - $MODULES_HTML)"
 
 popd < /dev/null
