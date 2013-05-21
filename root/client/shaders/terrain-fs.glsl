6c6764ae75f8706193be45a4b65e19907219ef9f
Andrew Rasmussen <arasmussen@katworks.com>
2013-05-21 08:31:10 -0700

    raise snow level a bit

--- root/client/shaders/terrain-fs.glsl
+++ root/client/shaders/terrain-fs.glsl
@@ -18,10 +18,10 @@ void main(void) {
     gl_FragColor = dirt;
   } else if (y < 0.0) {
     gl_FragColor = mix(dirt, grass, (y + 5.0) / 5.0);
-  } else if (y < 3.0) {
+  } else if (y < 2.0) {
     gl_FragColor = grass;
-  } else if (y < 5.0) {
-    gl_FragColor = mix(grass, snow, (y - 3.0) / 2.0);
+  } else if (y < 8.0) {
+    gl_FragColor = mix(grass, snow, (y - 2.0) / 6.0);
   } else {
     gl_FragColor = snow;
   }
