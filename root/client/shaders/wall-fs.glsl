b7082279777b33c15224ec10bda89b0fb07580d9
Andrew Rasmussen <arasmussen@katworks.com>
2013-05-27 19:09:14 -0700

    fix wall colors

--- root/client/shaders/wall-fs.glsl
+++ root/client/shaders/wall-fs.glsl
@@ -10,10 +10,10 @@ varying vec2 texCoord;
 void main(void) {
   vec4 color = texture2D(wood_texture, texCoord);
   if (build_state == 1) { // not built, but buildable
-    color.w = 0.8;
+    color.w = 0.9;
   } else if (build_state == 2) { // not built, not buildable
-    color.x = min(color.x + 0.15, 1.0);
-    color.w = 0.85;
+    color.x = min(color.x + 0.2, 1.0);
+    color.w = 0.9;
   }
   gl_FragColor = color;
 }
