b98819543eea71fb876a6d82d90e001d0acc4a43
Andrew Rasmussen <arasmussen@katworks.com>
2013-05-27 18:19:42 -0700

    add wall shaders

---
+++ root/client/shaders/wall-fs.glsl
@@ -0,0 +1,20 @@
+// Copyright (c) Venzio 2013 All Rights Reserved
+
+precision mediump float;
+
+uniform sampler2D wood_texture;
+uniform int build_state;
+
+varying vec2 texCoord;
+
+void main(void) {
+  vec4 color = texture2D(wood_texture, texCoord);
+  if (build_state == 1) { // not built, but buildable
+    color.w = 0.8;
+  } else if (build_state == 2) { // not built, not buildable
+    color.x = min(color.x + 0.15, 1.0);
+    color.w = 0.85;
+  }
+  gl_FragColor = color;
+}
+
