b10b2b74b4f4532750c9e56dfc6b31e1eb89eee1
Andrew Rasmussen <arasmussen@katworks.com>
2013-07-03 12:46:35 -0700

    use mongohq db

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,13 @@ define(['mongoose'], function(mongoose) {
     mongoose: mongoose,
 
     connect: function() {
-      mongoose.connect('mongodb://localhost/venzio');
+      var username = process.env.VENZIO_DB_USER;
+      var password = process.env.VENZIO_DB_PASS;
+      var host = process.env.VENZIO_DB_HOST;
+      var port = process.env.VENZIO_DB_PORT;
+      var name = process.env.VENZIO_DB_NAME;
+      var connectionString = 'mongodb://' + username + ':' + password + '@' + host + ':' + port + '/' + name;
+      mongoose.connect(connectionString);
       this.db = mongoose.connection;
       this.db.on('error', console.error.bind(console, 'connection error:'));
       this.db.once('open', this.success.bind(this));
