fccbdae31da3cec02f2451b0e9a008f82705567a
Andrew Rasmussen <arasmussen@katworks.com>
2013-07-03 18:06:28 -0700

    add sensitive constants

---
+++ server/web/sensitive_constants.js
@@ -0,0 +1,11 @@
+// Copyright (c) Venzio 2013 All Rights Reserved
+
+define(function() {
+  return {
+    DBNAME: '',
+    DBUSERNAME: '',
+    DBPASSWORD: '',
+    DBHOST: '',
+    DBPORT: ''
+  }
+});
