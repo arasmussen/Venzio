d0df86c83fb0a35c4d09b185df15d8930413e888
Andrew Rasmussen <andrew@venz.io>
2013-07-30 13:22:28 -0700

    make_subdomain script

---
+++ scripts/make_subdomain.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright (c) Venzio 2013 All Rights Reserved
+
+pushd `dirname $0` > /dev/null
+
+ROOT='../root'
+WWW="${ROOT}/www"
+SUBDOMAIN="${ROOT}/$1"
+
+if [ ! -d $SUBDOMAIN ]; then
+  mkdir $SUBDOMAIN
+  mkdir $SUBDOMAIN/img
+  mkdir $SUBDOMAIN/css
+  cp $WWW/img/headerLogo.png $SUBDOMAIN/img
+  cp $WWW/css/reset.css $SUBDOMAIN/css
+  cp $WWW/css/global.css $SUBDOMAIN/css/global.css
+  cp $WWW/analytics.js $SUBDOMAIN/analytics.js
+  cp $WWW/script.js $SUBDOMAIN/script.js
+  touch $SUBDOMAIN/index.html
+fi
+
+popd > /dev/null
