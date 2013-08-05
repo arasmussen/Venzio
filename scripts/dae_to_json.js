032b2912af704ad8bc40e8e1560270b7f64526cd
Andrew Rasmussen <andrew@venz.io>
2013-08-05 12:26:10 -0700

    some updates to dae_to_json

--- scripts/dae_to_json.js
+++ scripts/dae_to_json.js
@@ -1,7 +1,13 @@
+// imports
+
 var ejs = require('ejs');
 var fs = require('fs');
 
+// our template file
+
 var template = fs.readFileSync(__dirname + '/mesh_template.ejs', 'utf8');
+
+// make sure input file exists
 
 var input = process.argv[2];
 if (!input || !fs.existsSync(input) || input.substr(-4) != '.dae') {
@@ -9,9 +15,13 @@ if (!input || !fs.existsSync(input) || input.substr(-4) != '.dae') {
   return;
 }
 
+// compute output file name
+
 var start = input.lastIndexOf('/') == -1 ? 0 : input.lastIndexOf('/') + 1;
 var end = input.lastIndexOf('.');
 var output = input.substr(start, end - start) + '.json';
+
+// make sure output file doesn't already exist
 
 if (fs.existsSync(output)) {
   console.log('output file already exists');
@@ -90,11 +100,59 @@ for (var i = 0; i < index_data.v.length; i++) {
   buffer.t.push(data.t[2 * index_data.t[i] + 1]);
 }
 
+// get weights/joints data
+
+var weight_section_start = '<vertex_weights';
+
+var source_start = 'source="#';
+var source_end = '"';
+
+var joint_source = '<input semantic="JOINT"';
+var joints = [];
+var start = contents.indexOf(source_start, contents.indexOf(joint_source, contents.indexOf(weight_section_start))) + source_start.length;
+var end = contents.indexOf(source_end, start);
+var joints_label = contents.substr(start, end - start);
+var start = contents.indexOf('>', contents.indexOf(joints_label + '-array')) + 1;
+var end = contents.indexOf('<', start);
+var joints = contents.substr(start, end - start).trim().split(/[\s\n]+/);
+
+var weight_source = '<input semantic="WEIGHT"';
+var weights = [];
+var start = contents.indexOf(source_start, contents.indexOf(weight_source, contents.indexOf(weight_section_start))) + source_start.length;
+var end = contents.indexOf(source_end, start);
+var weights_label = contents.substr(start, end - start);
+var start = contents.indexOf('>', contents.indexOf(weights_label + '-array')) + 1;
+var end = contents.indexOf('<', start);
+var weights = contents.substr(start, end - start).trim().split(/[\s\n]+/);
+
+var weight_counts_start = '<vcount>';
+var start = contents.indexOf(weight_counts_start, contents.indexOf(weight_section_start)) + weight_counts_start.length;
+var end = contents.indexOf('<', start);
+var counts = contents.substr(start, end - start).trim().split(' ');
+
+var weight_indices_start = '<v>';
+var start = contents.indexOf(weight_indices_start, contents.indexOf(weight_section_start)) + weight_indices_start.length;
+var end = contents.indexOf('<', start);
+var indices = contents.substr(start, end - start).trim().split(' ');
+
+var vertices = [];
+var indices_index = 0;
+for (var i = 0; i < counts.length; i++) {
+  vertices[i] = {
+    influences: []
+  };
+  for (var j = 0; j < counts[i]; j++) {
+    vertices[i].influences.push({
+      joint: joints[indices[indices_index++]],
+      weight: weights[indices[indices_index++]]
+    });
+  }
+  console.log(vertices[i]);
+}
+
 var data = {
   v: buffer.v,
   n: buffer.n,
   t: buffer.t,
   count: buffer.v.length / 3
 };
-
-console.log(ejs.render(template, data));
